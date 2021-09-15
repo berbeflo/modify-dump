b45f2b8d95e3eb0b6b37a418c392ce54f06f845e
Florian Berberich <florian.berberich@gutzmann.com>
2021-09-15 11:37:36 +0200

    feat(): added more filters

---
+++ src/Filter/OnlyWithoutNoDumpAttribute.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Berbeflo\ModifyDump\Filter;
+
+use ReflectionProperty;
+use Berbeflo\ModifyDump\Attribute\NoDump;
+
+class OnlyWithoutNoDumpAttribute implements Filter
+{
+    public function isAllowed(ReflectionProperty $property, object $context) : bool
+    {
+        return count($property->getAttributes(NoDump::class)) === 0;
+    }
+}
